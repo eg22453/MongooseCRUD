e8f281a4284130a7e507a8ee4ad56f068c7f0fd8
Steven Garza (eg22453) <stevenmacgarza@gmail.com>
2019-08-03 16:21:13 -0500

    added cloud mongoose database and new user access

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set up mongoose connection
+let dev_db_url ='mongodb+srv://someuser:abcd1234@productcrudapp-axgmx.mongodb.net/test?retryWrites=true&w=majority';
+
+let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//end mongoose connection setup
+
+
 
 const product = require('./routes/product.route'); //Imports routes for product
 
