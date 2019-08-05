f76956152ffc1fc32308459852f058d40e767377
Steven Garza (eg22453) <stevenmacgarza@gmail.com>
2019-08-05 17:29:07 -0500

    fix create route and establish succesfull mongo connection

--- app.js
+++ app.js
@@ -9,17 +9,26 @@ const app = express();
 // Set up mongoose connection
 const mongoose = require('mongoose');
 let dev_db_url = 'mongodb://someuser:abcd1234@productcrudapp-shard-00-00-axgmx.mongodb.net:27017,productcrudapp-shard-00-01-axgmx.mongodb.net:27017,productcrudapp-shard-00-02-axgmx.mongodb.net:27017/test?ssl=true&replicaSet=ProductCRUDAPP-shard-0&authSource=admin&retryWrites=true&w=majority';
-let mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, {useNewUrlParser: true});
+//let mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(dev_db_url, {useNewUrlParser: true}).then(
+    () => {
+        console.log("Database connection established!");
+      },
+      err => {
+        console.log("Error connecting Database instance due to: ", err);
+      }
+);
 mongoose.Promise = global.Promise;
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+//end of mongoose connection!!
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/products', product);
 
-let port = 3001;
+let port = 3003;
 
 app.listen(port, () => {
     console.log('Server is up and running on port number ' + port);
